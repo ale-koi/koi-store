c1a8c7a45c97c1a01afe5fa15f9a15833447d4f9
Anyeli Adrianza <alx.adrz@gmail.com>
2024-09-20 18:44:31 -0400

    installing vite + sass, creating pages for needed components, editing styles

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './styles/App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>(ﾉ´ｰ`)ﾉ</h1>
+      <button>(⌒▽⌒)☆</button>
+    </>
+  )
+}
+
+export default App
