99b9f9a2ef7cedfa993dca62bfbdeca1ec927c3a
Anyeli Adrianza <alx.adrz@gmail.com>
2024-10-25 17:17:49 -0400

    minor fix

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react'
 import CartItem from './CartItem'
 import { Cart as CartContext} from '../contexts/CartProvider'
+import "../styles/cart.scss"
 
 const Cart = ({}) => {
     const {cartList, totalItem} = useContext(CartContext)
   return (
-    <div>
-        <CartItem product={cartList} />
+    <div className='main-cart'>
+        <h1> Cart!</h1>
+        <p>Your cart is currently empty</p>
+        <p>Wanna see more products?</p>
+        <button>Catalogue</button>
+        {/* <CartItem product={cartList} /> */}
     </div>
   )
 }
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,16 +3,17 @@ import '../styles/cartItem.scss'
 
 const CartItem = ({product}) => {
   return (
-    <div>
-        <div className="cart-products" id="">
-            <img src="https://picsum.photos/75/150"/>
-            <h2>Rekishi ni nokoru akujo ni naruzo akuyaku reijou ni naru hodo ouji no dekiai wa kasoku suru you desu. <span>vol. here.</span></h2>
-            <p>Qt. XX</p>
-            <p>Price: 0000</p>
-            <button>Remove</button>
-            
-        </div>
+    <>
+    <div className="cart-products" id="">
+        <img src="https://picsum.photos/75/150"/>
+        <h2>Rekishi ni nokoru akujo ni naruzo akuyaku reijou ni naru hodo ouji no dekiai wa kasoku suru you desu. <span>vol. here.</span></h2>
+        <p>Qt. XX</p>
+        <p>Price: 0000</p>
+        <button className='remove-icon'> x </button>
     </div>
+</>
+        
+
   )
 }
 
